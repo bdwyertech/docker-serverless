47772aeaeda60e986067f24a71ef0f28c9c7fab2
Brian Dwyer <Brian.Dwyer@broadridge.com>
2020-04-26 20:37:12 -0400

    Add a handler to preserve config
    Signed-off-by: Brian Dwyer <Brian.Dwyer@broadridge.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,6 +2,15 @@
 # Magic to Provision the Container
 # Brian Dwyer - Intelligent Digital Services
 
+# Config Handler
+finish() {
+  cp -f ~/.serverlessrc ~/.serverless/.serverlessrc
+}
+trap finish 0
+if [ -f ~/.serverless/.serverlessrc ]; then
+	cp -f ~/.serverless/.serverlessrc ~/.serverlessrc
+fi
+
 # Wrapper & Passthrough
 case "$1" in
 	* )	serverless "$@";;
